2c760819ce0920dd47238a93b797b680e99f7aca
Honorio Vega <hvega@csumb.edu>
2017-08-03 03:31:04 +0000

    changed app.py

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
-import os
-
-from flask import Flask, request, render_template, send_from_directory
-
-__author__ = 'ibininja'
-
+from flask import Flask
 app = Flask(__name__)
 
-
-
-@app.route("/")
-def index():
-    
-    return 'hello'
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
