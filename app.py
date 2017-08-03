d060a132e2f9aff914456d8fa04763ff8ebf78e0
Honorio Vega <hvega@csumb.edu>
2017-08-03 03:35:02 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return 'Hello World'
+
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
