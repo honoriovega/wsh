a65e426f65a5aa3447ed80b8172a0acde699dbfd
Honorio Vega <hvega@csumb.edu>
2017-08-02 20:25:35 -0700

    pushed files

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, request, render_template, send_from_directory
+
+__author__ = 'ibininja'
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    
+    return 'hello'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
